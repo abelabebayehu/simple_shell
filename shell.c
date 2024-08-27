87fa7754765241ea7680286572649a7a6d1d470e
Dr. Hiwot Wondale <hiwotwondalemd@gmail.com>
2024-08-28 01:09:39 +0300

    Task 1 - betty

---
+++ shell.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the shell program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Welcome to the simple shell!\n");
+	return (0);
+}
