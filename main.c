c58c8c053af3e2a134a954acccb2f92dc21cce31
abelabebayehu <abelperspectives@gmail.com>
2024-08-29 02:24:00 +0300

    main and memset

--- main.c
+++ main.c
@@ -1,20 +1,40 @@
 #include "shell.h"
 
 /**
- * main - Entry point of the shell
- * @argc: Argument count
- * @argv: Argument vector
- * @envp: Environment variables
+ * main - Entry point of the shell program
+ * @ac: Argument count
+ * @av: Argument vector
  *
- * Return: Always 0 (Success)
+ * Return: 0 on success, 1 on error
  */
-int main(int argc, char **argv, char **envp)
+
+int main(int ac, char **av)
 {
-	(void)argc;
-	(void)argv;
-	(void)envp;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-	display_prompt();
 
-	return (0);
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Unable to open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	shell_execute(info, av);
+	return (EXIT_SUCCESS);
 }
