41804e30f3fa1290d4ef151cc0cfb9e73e1c7ba0
Dr. Hiwot Wondale <hiwotwondalemd@gmail.com>
2024-08-28 04:48:33 +0300

    Delete everything in main

--- main.c
+++ main.c
@@ -8,33 +8,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, char **argv, char **envp)
+int main()
 {
-	char *input
-
-	(void)argc;
-
-	while (1)
-	{
-		display_prompt();
-		input = read_input();
-		if (input == NULL)
-		{
-			free(input);
-			break;
-		}
-
-		/* Trim newline character */
-		input[strcspn(input, "\n")] = '\0';
-
-		if (execute_command(input, envp) == -1)
-		{
-			/* Error handling */
-			fprintf(stderr, "%s: %s: No such file or directory\n", argv[0], input);
-		}
-
-		free(input);
-	}
-
+	
 	return (0);
 }
