bb427c85f03bc5316a07797a4f855e5416595592
Dr. Hiwot Wondale <hiwotwondalemd@gmail.com>
2024-08-28 05:29:04 +0300

    Replace printf with write

--- main.c
+++ main.c
@@ -8,8 +8,13 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(int argc, char **argv, char **envp)
 {
-	
+	(void)argc;
+	(void)argv;
+	(void)envp;
+
+	display_prompt();
+
 	return (0);
 }
